fe25319e8ebc33ed998c249c42fc5574753829e8
CRF41761 <crf.granja.saler@gmail.com>
2026-09-03 14:25:35 +0200

    Update message for cerdoVietnamitaNoRetenido

--- app.js
+++ app.js
@@ -1190,7 +1190,7 @@ const pantallas = {
   cerdoVietnamitaNoRetenido: {
     tipo: "fin",
     titulo: "🐷 Cerdo vietnamita suelto",
-    contenido: `<h3>No capturamos animales.</h3><p>Si no pueden retenerlo, llévelo a la policía local de su municipio y avísenos para que vayamos a recogerlo allí.</p><div class="contact-box"><strong> WhatsApp: 686 680 254</strong></div>`
+    contenido: `<h3>No cazamos animales.</h3><p>Si no pueden retenerlo, puededn contactar con el 112.</p><div class="contact-box"><strong> WhatsApp: 686 680 254</strong></div>`
   }
 
 };
